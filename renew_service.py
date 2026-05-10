eb45cd155756acc1554dbebafb02a433078aa443
VictorB <120435620+vb197701@users.noreply.github.com>
2026-05-10 09:50:52 +0000

    Update renew_service.py

--- renew_service.py
+++ renew_service.py
@@ -11,7 +11,6 @@ HIDENCLOUD_PASSWORD = os.environ.get('HIDENCLOUD_PASSWORD')
 BASE_URL = "https://dash.hidencloud.com"
 LOGIN_URL = f"{BASE_URL}/auth/login"
 SERVICE_URL = f"{BASE_URL}/service/211522/manage" # 请确认这是你的服务ID
-COOKIE_NAME = "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d"
 
 def log(message):
     print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {message}", flush=True)
